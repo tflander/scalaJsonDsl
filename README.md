dd870d893139a77829d07632e062afaeb5317ab2
Todd Flanders <tflander@wowway.com>
2013-11-01 12:43:39 -0400

    create failing test

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 Scala Json Dsl 
 ==============
 
-Note this project has branches step0 through stepN.  These branches form a step-by-step kata for test driving a scala-json parser.
+step 0
+------
+Done -- Created new scala project, configured the sbt-eclipse plug-in and updated scalatest version.
+
+Goal -- become familiar with the problem we are trying to solve
+
+Todo -- Review JsonParserTest and fix broken test.
+
+Notes
+------
+This project has branches step0 through stepN.  These branches form a step-by-step kata for test driving a scala-json parser.
 
 adopted from "Programming in Scala: A comprehensive step-by-step guide", 2nd edition 
 Martin Odersky, Lex Spoon, Bill Venners.
--- src/test/scala/toddf/json/JsonParserTest.scala
+++ src/test/scala/toddf/json/JsonParserTest.scala
@@ -1,5 +1,6 @@
 package toddf.json
 import org.scalatest._
+import scala.util.parsing.combinator.JavaTokenParsers
 
 class JsonParserTest extends FunSpec with ShouldMatchers {
 
@@ -8,17 +9,44 @@ from "Programming in Scala: A comprehensive step-by-step guide", 2nd edition
 Martin Odersky, Lex Spoon, Bill Venners
 published by Artima
 
+Here is the grammer for expressing JSON:
+
 value ::= obj | arr | stringLiteral | ßoatingPointNumber | "null" | "true" | "false".
-
-obj ::= "{" [members] "}".
-
-arr ::= "[" [values] "]".
-
-members ::= member {"," member}.
-
 member ::= stringLiteral ":" value.
-
+arr ::= "[" [values] "]".
+obj ::= "{" [members] "}".
+members ::= member {"," member}.
 values ::= value {"," value}.
 
    */
+
+  class Json extends JavaTokenParsers {
+    def value: Parser[Any] = "TODO: create lexical expression for parsing"
+    def obj: Parser[Any] = "TODO: create lexical expression for parsing"
+    def arr: Parser[Any] = "TODO: create lexical expression for parsing"
+    def member: Parser[Any] = "TODO: create lexical expression for parsing"
+    def values: Parser[Any] = "TODO: create lexical expression for parsing"
+    def members: Parser[Any] = "TODO: create lexical expression for parsing"
+  }
+
+  object ParseJson extends Json {
+
+    def parseJson(string: String) = {
+      parseAll(value, string)
+    }
+  }
+
+  import ParseJson._
+
+  describe("JSON parsing tests") {
+    it("parses empty Json") {
+      val json = parseJson("{}")
+      
+      // TODO:  Modify Json.value in the above class to make this test pass.
+      json.successful should be(true)
+      println()
+    }
+
+  }
+
 }
