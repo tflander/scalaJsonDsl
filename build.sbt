e11b00781459e6a5d8774829e1bcdc2562b186bd
Todd Flanders <tflander@wowway.com>
2013-11-01 11:48:21 -0400

    add readme

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := """scalaJsonDsl"""
+
+version := "1.0"
+
+scalaVersion := "2.10.2"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.0.M5b" % "test"
---
+++ src/test/scala/toddf/json/JsonParserTest.scala
@@ -0,0 +1,24 @@
+package toddf.json
+import org.scalatest._
+
+class JsonParserTest extends FunSpec with ShouldMatchers {
+
+  /*
+from "Programming in Scala: A comprehensive step-by-step guide", 2nd edition 
+Martin Odersky, Lex Spoon, Bill Venners
+published by Artima
+
+value ::= obj | arr | stringLiteral | ßoatingPointNumber | "null" | "true" | "false".
+
+obj ::= "{" [members] "}".
+
+arr ::= "[" [values] "]".
+
+members ::= member {"," member}.
+
+member ::= stringLiteral ":" value.
+
+values ::= value {"," value}.
+
+   */
+}
